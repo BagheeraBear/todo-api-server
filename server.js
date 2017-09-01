9be9ea671dd8ee92a47230ecf6a283cbfefe0d86
Ulf Swedin <ulf@swedin.nu>
2017-09-01 07:24:25 +0200

    Add POSt for todos

--- server.js
+++ server.js
@@ -1,23 +1,15 @@
 // Basic server
 // https://swedinserver-todo-api.herokuapp.com/
 var express=require('express');
+// middleware: body-parser
+var bodyParser = require('body-parser');
 var app=express();
 var PORT = process.env.PORT || 3000;
+var todos=[];
+var todoNextId = 1;
 
+app.use(bodyParser.json());
 
-var todos=[{
-  id: 1,
-  description: 'Meet mom for lunch',
-  completed: false
-},{
-  id:2,
-  description: 'Go to market',
-  completed: false
-},{
-  id:3,
-  description: 'Call my boss',
-  completed:true
-}];
 
 app.get('/', function(req, res){
   res.send('Todo API Root');
@@ -32,6 +24,7 @@ app.get('/todos', function(req,res){
 app.get('/todos/:id', function(req,res){
   //:id is available here
   var todoId=parseInt(req.params.id, 10);
+
   var matchedTodo;
 
   todos.forEach(function(todo){
@@ -61,6 +54,19 @@ app.get('/todos/:id', function(req,res){
   //res.send('Asking for a todo with id of ' + req.params.id);
 });
 
+// POST http-method /todos
+app.post('/todos', function(req, res){
+  var body=req.body;
+
+  // add id field
+  body.id=todoNextId++;
+  // push body into array
+  todos.push(body);
+
+  // Test: console.log('description: ' + body.description);
+
+  res.json(body);
+});
 
 app.listen(PORT, function(){
   console.log('Express listening on port ' + PORT + '!');
